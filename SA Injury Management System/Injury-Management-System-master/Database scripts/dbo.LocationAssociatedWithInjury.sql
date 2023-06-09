9c6ea5aff9c769e507aeca0e8ebf7d08b7e9ebd6
Kalpitpatel34 <135683000+Kalpitpatel34@users.noreply.github.com>
2023-06-09 12:41:03 +0930

    Delete SA Injury Management System/Injury-Management-System-master directory

--- SA Injury Management System/Injury-Management-System-master/Database scripts/dbo.LocationAssociatedWithInjury.sql
+++
@@ -1,8 +0,0 @@
-﻿CREATE TABLE [dbo].[LocationAssociatedWithInjury] (
-    [Id]                          INT  NOT NULL,
-    [PlaceWhereInjuryOccured]     TEXT NULL,
-    [SportOrRecreationalActivity] TEXT NULL,
-    PRIMARY KEY CLUSTERED ([Id] ASC),
-    CONSTRAINT [FK_LocationAssociatedWithInjury_UniqueIdentifiers] FOREIGN KEY ([Id]) REFERENCES [dbo].[UniqueIdentifiers] ([Id])
-);
-
--- SA Injury Management System/Injury-Management-System-master/Database scripts/dbo.SupplementaryInjuryMechanism.sql
+++
@@ -1,9 +0,0 @@
-﻿CREATE TABLE [dbo].[SupplementaryInjuryMechanism] (
-    [Id]                  INT  NOT NULL,
-    [InjuryMechanism]     TEXT NULL,
-    [ContributingFactors] TEXT NULL,
-    [AdditionalSuppData]  TEXT NULL,
-    PRIMARY KEY CLUSTERED ([Id] ASC),
-    CONSTRAINT [FK_SupplementaryInjuryMechanism_UniqueIdentifiers] FOREIGN KEY ([Id]) REFERENCES [dbo].[UniqueIdentifiers] ([Id])
-);
-
